64b7485c25b4cd8c825e44378a45e8869250815b
mmancino <none@none>
2010-10-10 13:40:20 +0200

    Angepasst...

--- src/main/java/de/mancino/armory/ArmoryTest.java
+++ src/main/java/de/mancino/armory/ArmoryTest.java
@@ -28,26 +28,41 @@ public class ArmoryTest  extends Armory {
 
     public ArmoryTest(String accountName, String password)
     throws ArmoryConnectionException {
-        super(accountName, password, "Serpentæ", "Forscherliga");
+        super(accountName, password, "Kawalsky", "Forscherliga");
         // TODO Auto-generated constructor stub
     }
 
     public static void main(String[] args) {
         try {
             final ArmoryTest aTest = new ArmoryTest(login, passwd);
-            /*
-            aTest.listPrices("King's Amber");
-            aTest.listPrices("Cardinal Ruby");
-            aTest.listPrices("Ametrine");
-            aTest.listPrices("Eye of Zul");
-            aTest.listPrices("Dreadstone");
-            */
+      
+//            aTest.listPrices("King's Amber");
+//            aTest.listPrices("Cardinal Ruby");
+//            aTest.listPrices("Ametrine");
+//            aTest.listPrices("Eye of Zul");
+//            aTest.listPrices("Dreadstone");
+   
             boolean reallyBuy = false;
-            aTest.buyAuctions("King's Amber", 870000, reallyBuy);
-            aTest.buyAuctions("Cardinal Ruby", 1030000, reallyBuy);
-            aTest.buyAuctions("Ametrine", 870000, reallyBuy);
-            aTest.buyAuctions("Dreadstone", 830000, reallyBuy);
-            aTest.buyAuctions("Eye of Zul", 650000, reallyBuy);
+       
+            aTest.buyAuctions("King's Amber", 910000, reallyBuy);
+//            aTest.buyAuctions("Cardinal Ruby", 990000, reallyBuy);
+//            aTest.buyAuctions("Ametrine", 870000, reallyBuy);
+//            aTest.buyAuctions("Dreadstone", 830000, reallyBuy);
+//            aTest.buyAuctions("Eye of Zul", 645000, reallyBuy);
+//            aTest.buyAuctions("Scarlet Ruby", 250000, reallyBuy);
+            
+
+            aTest.buyAuctions("Titanium Powder", 250000, reallyBuy);
+            aTest.buyAuctions("Titanium Ore", 50000, reallyBuy);
+            //aTest.listPrices("Haunted Memento");
+            aTest.listPrices("Haunted Memento");
+            //aTest.listPrices("Damaged Necklace");
+            aTest.listPrices("Titanium Powder");
+            aTest.listPrices("Titanium Ore");
+     
+            //aTest.listPrices("Titanium Powder");
+            //aTest.listPrices("Titanium Ore");
+            //aTest.listPrices("King's Amber");
         } catch (ArmoryConnectionException e) {
             e.printStackTrace();
         }
@@ -57,14 +72,14 @@ public class ArmoryTest  extends Armory {
     }
 
     public void buyAuctions(final String name, final long maxPrice, final boolean reallyBuy) throws ArmoryConnectionException {
-        AuctionSearch search = searchAuction(name, Quality.RARE, true);
+        AuctionSearch search = searchAuction(name, Quality.COMMON, true);
         Collections.sort(search.auctionItems, new AuctionItemByUnitBuyoutPrice());
         final long cheapest  =  search.auctionItems.get(0).buy / search.auctionItems.get(0).quantity;
         LOG.info("Buying '{}' which is cheaper than {} - Found {} Items, cheapest {}",
-                new Object[]{name, maxPrice, search.auctionItems.size(), cheapest});
+                new Object[]{name, toGold(maxPrice), search.auctionItems.size(), toGold(cheapest)});
         for(AuctionItem item : search.auctionItems) {
             if((item.buy/item.quantity) <= maxPrice) {
-                final String msg = item.name + "(" + item.quantity + "): " + item.buy + " - " + item.auctionId;
+                final String msg = item.name + "(" + item.quantity + "): " + toGold(item.buy) + " - " + item.auctionId;
                 if(reallyBuy) {
                     LOG.warn("Buying: " + msg);
                     buyAuction(item);
@@ -75,15 +90,22 @@ public class ArmoryTest  extends Armory {
         }
     }
 
+    public String toGold(long price) {
+        long copper = price %100;
+        long silver = (price/100) % 100;
+        long gold = (price/10000);
+        return gold + "g" + silver + "s" + copper + "c";
+    }
+    
     public void listPrices(final String name) throws ArmoryConnectionException {
         final StringBuffer sb = new StringBuffer();
-        AuctionSearch search = searchAuction(name, Quality.RARE, true);
+        AuctionSearch search = searchAuction(name, Quality.COMMON, true);
         Collections.sort(search.auctionItems, new AuctionItemByUnitBuyoutPrice());
         sb.append(search.auctionItems.size()).append(" Auctions for '").append(name)
             .append("', sorted by price/unit:\n");
         for(AuctionItem item : search.auctionItems) {
-            sb.append(" * ").append(item.quantity).append(" ").append(name).append(" ")
-                .append(item.buy/item.quantity).append(" each\n");
+            sb.append(" * ").append(item.quantity).append(" ").append(name).append(" at ").append(toGold(item.buy)).append(" (")
+                .append(toGold(item.buy/item.quantity)).append(" each)\n");
         }
 
         LOG.info(sb.toString());
