911bf73bd1f367905ad6c8d8a8ac1955f2e98ad3
mmancino <none@none>
2010-10-14 14:56:06 +0200

    * JavaDoc  * SerialVersionUID  * alte Exception entfernt

--- src/main/java/de/mancino/exceptions/ArmoryConnectionException.java
+++ src/main/java/de/mancino/exceptions/ArmoryConnectionException.java
@@ -1,20 +1,40 @@
-/*
- * ArmoryConnectionException.java 15.03.2010
- *
- * Copyright (c) 2010 1&1 Internet AG. All rights reserved.
- *
- * $Id$
- */
 package de.mancino.exceptions;
 
+/**
+ * Error connecting to armory.
+ *
+ * @author mmancino
+ */
 public class ArmoryConnectionException extends Exception {
+    /**
+     * Serial Version UID
+     */
+    private static final long serialVersionUID = 1L;
 
+    /**
+     * Error connecting to armory.
+     *
+     * @param e cause
+     */
     public ArmoryConnectionException(Exception e) {
         super(e);
     }
+
+    /**
+     * Error connecting to armory.
+     *
+     * @param msg message
+     * @param e cause
+     */
     public ArmoryConnectionException(String msg, Exception e) {
         super(msg, e);
     }
+
+    /**
+     * Error connecting to armory.
+     *
+     * @param msg message
+     */
     public ArmoryConnectionException(String msg) {
         super(msg);
     }
--- src/main/java/de/mancino/exceptions/ArmoryRequestError.java
+++ src/main/java/de/mancino/exceptions/ArmoryRequestError.java
@@ -1,15 +1,25 @@
-/*
- * ArmoryRequestError.java 11.10.2010
- *
- * Copyright (c) 2010 1&1 Internet AG. All rights reserved.
- *
- * $Id$
- */
 package de.mancino.exceptions;
 
 import de.mancino.armory.xml.ArmoryError;
 
+/**
+ * Armory Request Error.
+ * Armory did respond correctly, but an error-message was given
+ *
+ * @author mmancino
+ */
 public class ArmoryRequestError extends ArmoryConnectionException {
+    /**
+     * Serial Version UID
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Armory Request Error.
+     * Armory did respond correctly, but an error-message was given
+     *
+     * @param error Armory Error
+     */
     public ArmoryRequestError(ArmoryError error) {
         super("Error " + error.code + ": " + error.message);
     }
