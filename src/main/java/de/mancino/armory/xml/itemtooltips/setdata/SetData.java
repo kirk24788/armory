754969ba012cadcf6472f6c7d9e725932e45e2a6
mmancino <none@none>
2010-10-21 17:04:36 +0200

    ueberlange zeile gefixed
    --HG--
    branch : dev

--- src/main/java/de/mancino/armory/xml/itemtooltips/setdata/SetData.java
+++ src/main/java/de/mancino/armory/xml/itemtooltips/setdata/SetData.java
@@ -12,7 +12,8 @@ import javax.xml.bind.annotation.XmlElement;
         <item name="Crimson Acolyte Shoulderpads"/>
         <item name="Crimson Acolyte Gloves"/>
         <item name="Crimson Acolyte Hood"/>
-        <setBonus desc="Your Flash Heal has a 33% chance to cause the target to heal for 33% of the healed amount over 9 sec." threshold="2"/>
+        <setBonus desc="Your Flash Heal has a 33% chance to cause the target to heal for 33% of the healed amount over 9 sec." 
+                  threshold="2"/>
         <setBonus desc="Increases the effect of Power Word: Shield by 5% and Circle of Healing by 10%." threshold="4"/>
       </setData>
 
