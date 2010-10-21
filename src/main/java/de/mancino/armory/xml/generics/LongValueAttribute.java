b3478c15c3f1f912be79eee5432b00453b953e06
mmancino <none@none>
2010-10-21 17:11:47 +0200

    javedoc für generische attribute
    --HG--
    branch : dev

--- src/main/java/de/mancino/armory/xml/generics/LongValueAttribute.java
+++ src/main/java/de/mancino/armory/xml/generics/LongValueAttribute.java
@@ -2,7 +2,15 @@ package de.mancino.armory.xml.generics;
 
 import javax.xml.bind.annotation.XmlAttribute;
 
+/**
+ * Generic Long value attribute.
+ * 
+ * @author mario
+ */
 public class LongValueAttribute {
+    /**
+     * Long attribute
+     */
     @XmlAttribute
     public long value;
 }
--- src/main/java/de/mancino/armory/xml/generics/StringValueAttribute.java
+++ src/main/java/de/mancino/armory/xml/generics/StringValueAttribute.java
@@ -2,7 +2,15 @@ package de.mancino.armory.xml.generics;
 
 import javax.xml.bind.annotation.XmlAttribute;
 
+/**
+ * Generic String value attribute.
+ * 
+ * @author mario
+ */
 public class StringValueAttribute {
+    /**
+     * String attribute.
+     */
     @XmlAttribute
     public String value;
 }
